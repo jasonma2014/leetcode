4e094a2bd8f6edec353f26066d5fd5adf05a9a2d
Jason Ma <majian2014@foxmail.com>
2021-08-19 09:50:44 +0800

    Add Reverse Vowels.

---
+++ src/com/leetcode/algo/p345/Solution.java
@@ -0,0 +1,34 @@
+package com.leetcode.algo.p345;
+
+public class Solution {
+    //利用双指针逼近, 不管前面还是后面碰到元音,就等着后面指针找到,所以需要两个循环来找到i,j的元音
+    public String reverseVowels(String s) {
+        int n = s.length();
+        char[] arr = s.toCharArray();
+        int i = 0, j = n - 1;
+        while (i < j) {
+            while (i < n && !isVowel(arr[i])) {
+                ++i;
+            }
+            while (j > 0 && !isVowel(arr[j])) {
+                --j;
+            }
+            if (i < j) {
+                swap(arr, i, j);
+                ++i;
+                --j;
+            }
+        }
+        return new String(arr);
+    }
+
+    public boolean isVowel(char ch) {
+        return "aeiouAEIOU".indexOf(ch) >= 0;
+    }
+
+    public void swap(char[] arr, int i, int j) {
+        char temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
