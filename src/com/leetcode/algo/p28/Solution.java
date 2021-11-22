951bbdbf34b89aec2c54c3310c3dcd5407e7d713
majian <majian2014@foxmail.com>
2021-11-22 14:01:04 +0800

    add strstr solution.

---
+++ src/com/leetcode/algo/p28/Solution.java
@@ -0,0 +1,11 @@
+package com.leetcode.algo.p28;
+
+public class Solution {
+    public int strStr(String haystack, String needle) {
+        if (needle.length()  == 0) {
+            return 0;
+        int window = needle.length();
+        
+
+    }
+}
