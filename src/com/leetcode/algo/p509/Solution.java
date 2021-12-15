5cba9125784906c6d4030ff723a7c744ec8dbe8c
majian <majian2014@foxmail.com>
2021-12-15 16:12:57 +0800

    update the solution.

--- src/com/leetcode/algo/p509/Solution.java
+++ src/com/leetcode/algo/p509/Solution.java
@@ -2,14 +2,11 @@ package com.leetcode.algo.p509;
 
 public class Solution {
     public int fib(int n) {
-        if (n == 0 ) {
-            return 0;
+        if (n == 1 || n == 2) {
+            return n;
         }
 
-        if ( n == 1 || n == 2) {
-            return 1;
-        }
-        int prev = 1, curr = 1;
+        int prev = 1, curr = 2;
         for (int i = 3; i <= n; i++) {
             int sum = prev + curr;
             prev = curr;
