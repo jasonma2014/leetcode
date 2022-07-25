761d7451df48185d4c7c5a9d39b7c9cd5c531258
Jian Ma <majian01@agree.com.cn>
2022-07-25 14:23:18 +0800

    add running sum.

---
+++ src/com/leetcode/algo/p1480/Solution.java
@@ -0,0 +1,14 @@
+package com.leetcode.algo.p1480;
+
+public class Solution {
+    public int[] runningSum(int[] nums) {
+        int len = nums.length;
+        int[] res = new int[len];
+        int sum = 0;
+        for (int i = 0; i < len; i++) {
+            sum += nums[i];
+            res[i] = sum;
+        }
+        return res;
+    }
+}
