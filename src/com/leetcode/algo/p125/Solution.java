209c7cffb883e5cdd2aae614e53c45c749caaf10
Jian Ma <majian01@agree.com.cn>
2022-09-11 00:38:31 +0800

    finished the p125 valid palindrome.

---
+++ src/com/leetcode/algo/p125/Solution.java
@@ -0,0 +1,24 @@
+package com.leetcode.algo.p125;
+
+public class Solution {
+    public boolean isPalindrome(String s) {
+        int n = s.length();
+        int left = 0, right = n - 1;
+        while (left < right) {
+            while (left < right && !Character.isLetterOrDigit(s.charAt(left))) {
+                ++left;
+            }
+            while (left < right && !Character.isLetterOrDigit(s.charAt(right))) {
+                --right;
+            }
+            if (left < right) {
+                if (Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))) {
+                    return false;
+                }
+                ++left;
+                --right;
+            }
+        }
+        return true;
+    }
+}
---
+++ src/com/leetcode/algo/p6/Solution.java
@@ -0,0 +1,4 @@
+package com.leetcode.algo.p6;
+
+public class Solution {
+}
